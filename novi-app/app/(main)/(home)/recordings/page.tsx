6eab03f429adbd1395a3a6b3002ca890253fe499
Chathila_Wijesinghe <chathilawijesinghe@gmail.com>
2026-02-23 21:05:56 +0530

    Removed Stream-io left overs

--- novi-app/app/(main)/(home)/recordings/page.tsx
+++ novi-app/app/(main)/(home)/recordings/page.tsx
@@ -1,11 +1,7 @@
-import CallList from "@/components/CallList";
-
 const recordings =() =>{
     return (
         <section className="flex size-full flex-col gap-10 text-white animate-fade-in">
-           <h1 className="text-3xl text-black text-center mt-3">recordings</h1>
-
-            <CallList type="recordings"/>
+           <h1 className="text-3xl text-black text-center mt-3">Under Development</h1>
       
         </section>
     );
--- novi-app/app/(main)/(home)/upcoming/page.tsx
+++ novi-app/app/(main)/(home)/upcoming/page.tsx
@@ -1,13 +1,10 @@
-import CallList from "@/components/CallList";
-
-
 const upcoming =()=>{
     return(
         <section className="flex size-full flex-col gap-10 animate-fade-in">
-            <h1 className="text-3xl text-black text-center mt-3">upcoming meetings
+            <h1 className="text-3xl text-black text-center mt-3">Under Development
 
             </h1>
-            <CallList type="upcoming"/>
+           
 
         </section>
     );
